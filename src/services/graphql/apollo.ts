624f77ad3bfb28b339f625b05586015c9f68b40b
Pushpendra Mishra <pushpendra.mishra660@gmail.com>
2023-10-16 18:43:26 +0530

    env configuration added.

--- src/services/graphql/apollo.ts
+++ src/services/graphql/apollo.ts
@@ -1,10 +1,12 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
+import Config from "react-native-config";
+
 
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   cache,
-  uri: 'http://localhost:9002/graphql',
+  uri:  Config.API_URL,
 });
 
 export default client;
